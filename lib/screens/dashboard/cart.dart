db3962025d6a88ada21ff87b2dc9c14ebf93e9b8
Chuckibmk <tsartobnrys@gmail.com>
2024-08-23 14:09:13 +0100

    cart update

--- lib/screens/dashboard/cart.dart
+++ lib/screens/dashboard/cart.dart
@@ -2,7 +2,7 @@ import 'package:counter_button/counter_button.dart';
 import 'package:eatup/screens/account/login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
